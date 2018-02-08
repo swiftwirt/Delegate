c1672e4282bda23c54d429283ca72ede47ba4f55
Dmitry Ivashin <dmitry@lunapps.com>
2018-02-08 14:33:23 +0200

    back up

---
+++ Delegate/Presentation/Tasks/TeamCell.swift
@@ -0,0 +1,17 @@
+//
+//  TeamCell.swift
+//  Delegate
+//
+//  Created by Dmitry Ivashin on 2/8/18.
+//  Copyright © 2018 Dmitry Ivashin. All rights reserved.
+//
+
+import UIKit
+
+class TeamCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
